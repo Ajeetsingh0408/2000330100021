f2d1425f362c48a824bd8c6638cca1861e977f68
Ajeetsingh0408 <138655279+Ajeetsingh0408@users.noreply.github.com>
2023-07-22 11:39:28 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import TrainSchedulePage from './pages/TrainSchedulePage';
+
+function App() {
+  return (
+    <div className="App">
+      <TrainSchedulePage />
+    </div>
+  );
+}
+
+export default App;
